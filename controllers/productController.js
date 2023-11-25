fbff0b3a56c68ec9e3d016cabfe19f1717912c5d
Roland-Sankara <roland1sankara@gmail.com>
2023-11-25 22:59:03 +0300

    update product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,7 @@ const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient();
 
 async function getProducts(req, res){
-    const products = await prisma.product.findMany({})
+    const products = await prisma.product.findMany()
     res.status(200).json({message: "success", products})
 }
 

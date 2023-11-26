af687fa95b2a341806866dd76f5cda0ac3a704ff
Roland-Sankara <roland1sankara@gmail.com>
2023-11-26 12:25:59 +0300

    update categories controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,13 @@
-function getCategories(req, res){
-    res.send("Here are the Categories")
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient();
+
+async function getCategories(req, res){
+    try {
+        const categories = await prisma.category.findMany();
+        res.status(200).json({message: "Success", categories});
+    } catch (error) {
+        res.status(400).json({error})
+    }
 }
 
 module.exports = getCategories;
